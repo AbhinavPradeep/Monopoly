732157930e48eb2c48091ff78269dd0d0519c26d
Abhinav Pradeep <abhinav.pradeep@hotmail.com>
2017-10-08 19:07:58 +0530

    Inital Commit
    Creating Banker Lucy!! Yay!!

---
+++ Monopoly_Banker/Banker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly_Banker
+{
+    class Banker
+    {
+        int balance ;
+
+        public Banker()
+        {
+            Console.WriteLine("Hello I am your banker Lucy, Welcome Bow WOW !!!");
+            balance = 2500 ;
+            Console.WriteLine("I am giving you " + balance + " to start the game");
+        }
+
+        public bool Withdraw (int WAmount)
+        {
+            if (balance >= WAmount)
+            {
+                balance = balance - WAmount;
+                Console.WriteLine("You have Withdrawn " + WAmount);
+                return true ;
+            }
+            else
+            {
+                return false ;
+            }
+           
+        }
+
+        public void Deposit(int DAmount)
+        {
+            balance = balance + DAmount;
+            Console.WriteLine("You have deposited " + DAmount);
+        }
+
+        public int ShowBalance()
+        {
+            Console.WriteLine("Your banlance is " + balance) ;
+            return balance;
+
+        }
+    }
+}
---
+++ Monopoly_Banker/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly_Banker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response;
+
+            Banker lucy = new Banker();
+
+            do
+            {
+                Console.WriteLine("Do you want to (D)eposit money or (W)ithdraw money or (S)ee your balance or (Q)uit");
+                response = Console.ReadLine();
+
+                if (response == "D" )
+                {
+                    int DAmount;
+                    string SAmount;
+
+                    Console.WriteLine( " Enter the amount you want to deposit " );
+                    SAmount = Console.ReadLine();
+                    int.TryParse(SAmount, out DAmount);
+
+                    lucy.Deposit(DAmount);
+
+                }
+                else  if (response == "W")
+                {
+                    int WAmount;
+                    string WSAmount;
+
+                    Console.WriteLine(" Enter the amount you want to withdraw ");
+                    WSAmount = Console.ReadLine();
+                    int.TryParse(WSAmount, out WAmount);
+
+                    if( lucy.Withdraw(WAmount) == false)
+                        Console.WriteLine("Withdrawal failed");
+
+                }
+                else if (response == "S")
+                {
+                    lucy.ShowBalance();
+                }
+
+
+            } while (response != "Q");
+
+            Environment.Exit(0);
+        }
+    }
+}
